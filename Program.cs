904e219aa388e547df43e60b94b388b0fc127138
Raymond Arceneaux <raymond.techcoder@gmail.com>
2020-03-21 20:22:59 -0500

    added planets

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Planets_Spaceships
 {
@@ -6,7 +8,35 @@ namespace Planets_Spaceships
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            string demPlanets = string.Join(",", planetList);
+            Console.WriteLine(demPlanets);
+
+            var planetList2 = new List<string>() {"Neptune","Uranus"};
+            planetList.AddRange(planetList2);
+            planetList.Insert(0,"Earth");
+            planetList.Insert( 7,"Venus");
+            planetList.Add("Pluto");
+
+            var myPlanets = planetList.GetRange(0, 9);
+            var rockyPlanets = new List<string>(){"Earth","Mercury","Venus","Mars"};
+            myPlanets.Remove("Pluto");
+
+            //Dictionary//
+            
+            var spacecrafts = new Dictionary<string,List<string>>();
+
+            spacecrafts.Add("SpaceX", new List<string>(){"Mars","Saturn","Mercury"});
+            spacecrafts.Add("BlueOrigin", new List<string>(){"Venus","Neptune"});
+
+            foreach (var (spacecraft,planet) in planetList)
+            {
+                Console.WriteLine($"{spacecraft} {planet}");
+            }
         }
     }
 }
